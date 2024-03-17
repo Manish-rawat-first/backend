a8652e1dad0960bbf180541316bd72ae15401033
Manish-rawat-first <manishrawat4876@gmail.com>
2024-03-17 23:10:47 +0530

    Update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {User} from "../models/user.model.js";
-import {uploadOnCloudinary} from "../utils/cloudinary";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
@@ -20,7 +20,7 @@ const registerUser = asyncHandler(async(req,res)=>{
     if([fullname,email,username,password].some(field=>field?.trim() ==="")){
         throw new ApiError(400,"All fields are required")
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username}, {email}]
     })
     if(existedUser){
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import {UploadStream, v2 as cloudinary} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 
@@ -26,3 +26,4 @@ cloudinary.config({
         return null;
     }
   }
+  export {uploadOnCloudinary}
