9de7756cb84d077e9493e3b4274c29ecb851bf9c
Manish-rawat-first <manishrawat4876@gmail.com>
2024-03-17 13:44:54 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -13,5 +13,12 @@ app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
- 
+
+// routes implement
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/register
 export {app}
