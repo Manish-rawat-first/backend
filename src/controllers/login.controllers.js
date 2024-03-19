cbec4ce92270742301f26f7516c88fbfb30a5f85
Manish-rawat-first <manishrawat4876@gmail.com>
2024-03-19 12:50:08 +0530

    com

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -6,7 +6,6 @@ import { generateAccessAndRefreshToken } from "./user.controllers.js";
 
 const loginUser = asyncHandler(async (req, res) =>{
     const {email, username, password} = req.body
-    console.log(email);
 
     if (!username && !email) {
         throw new ApiError(400, "username or email is required")
